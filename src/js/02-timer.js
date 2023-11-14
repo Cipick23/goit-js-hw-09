bebd90422ec660960e18edb45b32b8991afc29f8
Cipick23 <133102658+Cipick23@users.noreply.github.com>
2023-11-14 18:00:50 +0200

    error fix

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,7 +1,5 @@
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
-import Notiflix from 'notiflix';
-import 'notiflix/dist/notiflix-3.2.6.min.css';
 
 const dateInput = document.getElementById('datetime-picker');
 const dateBtn = document.getElementsByClassName('startBtn')[0];
@@ -22,7 +20,6 @@ const currentDatetime = `${currentYear}-${currentMonth}-${currentDay} ${currentH
 
 dateInput.value = currentDatetime;
 
-// Funcție pentru a calcula timpul rămas între două date
 function calculateTimeLeft(targetDate) {
   const now = new Date();
   const target = new Date(targetDate);
@@ -68,31 +65,25 @@ dateBtn.addEventListener('click', () => {
   const selectedDate = new Date(dateInput.value);
 
   if (selectedDate && selectedDate > currentDate) {
-    // Calculați timpul rămas
     const timeLeft = calculateTimeLeft(selectedDate);
 
-    // Actualizați conținutul elementelor HTML
     daysElement.textContent = timeLeft.days.toString().padStart(2, '0');
     hoursElement.textContent = timeLeft.hours.toString().padStart(2, '0');
     minutesElement.textContent = timeLeft.minutes.toString().padStart(2, '0');
     secondsElement.textContent = timeLeft.seconds.toString().padStart(2, '0');
 
-    // Oprește numărătoarea inversă dacă a ajuns la 00:00:00:00
     if (timeLeft.total <= 0) {
       clearInterval(intervalId);
     } else {
-      // Inițiați numărătoarea inversă dacă nu este deja pornită
       if (!intervalId) {
         intervalId = setInterval(() => {
           const timeLeft = calculateTimeLeft(selectedDate);
 
-          // Actualizați conținutul elementelor HTML
           daysElement.textContent = timeLeft.days.toString().padStart(2, '0');
           hoursElement.textContent = timeLeft.hours.toString().padStart(2, '0');
           minutesElement.textContent = timeLeft.minutes.toString().padStart(2, '0');
           secondsElement.textContent = timeLeft.seconds.toString().padStart(2, '0');
 
-          // Oprește numărătoarea inversă dacă a ajuns la 00:00:00:00
           if (timeLeft.total <= 0) {
             clearInterval(intervalId);
           }
