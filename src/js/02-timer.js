e8c35db25d734e0b2d3b867dd8b4f81f3e3b4b22
Cipick23 <133102658+Cipick23@users.noreply.github.com>
2023-11-14 11:51:15 +0200

    live server error

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,82 +1,103 @@
-import flatpickr from "/node_modules/flatpickr";
-import "/node_modules/flatpickr/dist/flatpickr.min.css";
-import Notiflix from '/node_modules/notiflix';
-import "/node_modules/notiflix/dist/notiflix-3.2.6.min.css";
+import flatpickr from 'flatpickr';
+// import 'flatpickr/dist/flatpickr.min.css';
+import Notiflix from 'notiflix';
+// import 'notiflix/dist/notiflix-3.2.6.min.css';
 
 const dateInput = document.getElementById('datetime-picker');
-const dateValue = document.getElementsByClassName('value');
-const dateLabel = document.getElementsByClassName('label');
-const dateBtn = document.getElementsByClassName('startBtn');
+const dateBtn = document.getElementsByClassName('startBtn')[0];
+const daysElement = document.querySelector('[data-days]');
+const hoursElement = document.querySelector('[data-hours]');
+const minutesElement = document.querySelector('[data-minutes]');
+const secondsElement = document.querySelector('[data-seconds]');
+let intervalId;
 
-const options = {
+const currentDate = new Date();
+const currentYear = currentDate.getFullYear();
+const currentMonth = (currentDate.getMonth() + 1).toString().padStart(2, '0');
+const currentDay = currentDate.getDate().toString().padStart(2, '0');
+const currentHour = currentDate.getHours().toString().padStart(2, '0');
+const currentMinute = currentDate.getMinutes().toString().padStart(2, '0');
+
+const currentDatetime = `${currentYear}-${currentMonth}-${currentDay} ${currentHour}:${currentMinute}`;
+
+dateInput.value = currentDatetime;
+
+// Funcție pentru a calcula timpul rămas între două date
+function calculateTimeLeft(targetDate) {
+  const now = new Date();
+  const target = new Date(targetDate);
+  const diff = target - now;
+
+  if (diff <= 0) {
+    return { days: '00', hours: '00', minutes: '00', seconds: '00', total: 0 };
+  }
+
+  const seconds = Math.floor(diff / 1000) % 60;
+  const minutes = Math.floor(diff / (1000 * 60)) % 60;
+  const hours = Math.floor(diff / (1000 * 60 * 60)) % 24;
+  const days = Math.floor(diff / (1000 * 60 * 60 * 24));
+
+  return {
+    days: days.toString().padStart(2, '0'),
+    hours: hours.toString().padStart(2, '0'),
+    minutes: minutes.toString().padStart(2, '0'),
+    seconds: seconds.toString().padStart(2, '0'),
+    total: diff,
+  };
+}
+
+dateInput.addEventListener('click', () => {
+  flatpickr(dateInput, {
     enableTime: true,
     time_24hr: true,
-    defaultDate: new Date(),
-    minuteIncrement: 1,
     onClose(selectedDates) {
-      console.log(selectedDates[0]);
-    },
-  };
+      const currentDate = new Date();
+      const selectedDate = selectedDates && selectedDates[0];
 
-    console.log(new Date())
-
-  let isTimerRunning = true;
-    
-dateInput.addEventListener('click', () => {
-  setTimeout(() => {
-      flatpickr(dateInput, options);
-      if (new Date() <= options.defaultDate) {
-          Notiflix.Notify.warning('Please choose a date in the future!');
-      };
-      if (!selectedDates[0]) {
-        isTimerRunning = true;
+      if (selectedDate && selectedDate > currentDate) {
+        dateBtn.disabled = false;
+      } else {
+        dateBtn.disabled = true;
+        Notiflix.Notify.warning('Please choose a date in the future!');
       }
-  }, 0); // Adăugați un timp de întârziere, de exemplu, 0
+    },
+  });
 });
 
+dateBtn.addEventListener('click', () => {
+  const selectedDate = new Date(dateInput.value);
 
-//   dateBtn.addEventListener('click', () => {
+  if (selectedDate && selectedDate > currentDate) {
+    // Calculați timpul rămas
+    const timeLeft = calculateTimeLeft(selectedDate);
 
-//   })
+    // Actualizați conținutul elementelor HTML
+    daysElement.textContent = timeLeft.days.toString().padStart(2, '0');
+    hoursElement.textContent = timeLeft.hours.toString().padStart(2, '0');
+    minutesElement.textContent = timeLeft.minutes.toString().padStart(2, '0');
+    secondsElement.textContent = timeLeft.seconds.toString().padStart(2, '0');
 
-// dateValue.addEventListener('input', () => {
-//     if (options) {
-//         options(); // folosește variabila options în funcție
-//     }
-// });
+    // Oprește numărătoarea inversă dacă a ajuns la 00:00:00:00
+    if (timeLeft.total <= 0) {
+      clearInterval(intervalId);
+    } else {
+      // Inițiați numărătoarea inversă dacă nu este deja pornită
+      if (!intervalId) {
+        intervalId = setInterval(() => {
+          const timeLeft = calculateTimeLeft(selectedDate);
 
-//   dateLabel.addEventListener('input', () => {
-//     options();
-//   });
+          // Actualizați conținutul elementelor HTML
+          daysElement.textContent = timeLeft.days.toString().padStart(2, '0');
+          hoursElement.textContent = timeLeft.hours.toString().padStart(2, '0');
+          minutesElement.textContent = timeLeft.minutes.toString().padStart(2, '0');
+          secondsElement.textContent = timeLeft.seconds.toString().padStart(2, '0');
 
-//   function WindowAlert() {
-//     console.info("Please choose a date in the future!");
-//   };
-
-
-  function convertMs(ms) {
-    // Number of milliseconds per unit of time
-    const second = 1000;
-    const minute = second * 60;
-    const hour = minute * 60;
-    const day = hour * 24;
-  
-    // Remaining days
-    const days = Math.floor(ms / day);
-    // Remaining hours
-    const hours = Math.floor((ms % day) / hour);
-    // Remaining minutes
-    const minutes = Math.floor(((ms % day) % hour) / minute);
-    // Remaining seconds
-    const seconds = Math.floor((((ms % day) % hour) % minute) / second);
-  
-    return { days, hours, minutes, seconds };
-  };
-  
-  console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
-  console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
-  console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
-
-  function addLeadingZero(_value) {
-    padStart();
-  };
+          // Oprește numărătoarea inversă dacă a ajuns la 00:00:00:00
+          if (timeLeft.total <= 0) {
+            clearInterval(intervalId);
+          }
+        }, 1000);
+      }
+    }
+  }
+});
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,44 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
-}
+import Notiflix from '/node_modules/notiflix';
+  
+  // Asigurați-vă că Notiflix este încărcat înainte de a utiliza funcționalitatea
+  // Notiflix.Notify este folosit pentru a afișa notificări către utilizator
+  // Scripturile Notiflix pot fi incluse prin adăugarea unor script-uri precum cele de mai sus
+
+  document.getElementById('promiseForm').addEventListener('submit', function (event) {
+    event.preventDefault(); // Preveniți comportamentul implicit de trimitere a formularului
+
+    // Obțineți valorile din câmpurile formularului
+    const delay = parseInt(this.querySelector('[name="delay"]').value);
+    const step = parseInt(this.querySelector('[name="step"]').value);
+    const amount = parseInt(this.querySelector('[name="amount"]').value);
+
+    // Apelează funcția createPromise pentru fiecare promisiune și afișează notificări către utilizator
+    for (let i = 1; i <= amount; i++) {
+      const position = i;
+      const currentDelay = delay + (i - 1) * step;
+
+      createPromise(position, currentDelay)
+        .then(({ position, delay }) => {
+          Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+        })
+        .catch(({ position, delay }) => {
+          Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+        });
+    }
+  });
+
+  function createPromise(position, delay) {
+    // Returnați un promise care va fi executat sau respins după un delay
+    return new Promise((resolve, reject) => {
+      const shouldResolve = Math.random() > 0.3;
+
+      // Utilizați setTimeout pentru a întârzia executarea sau respingerea promise-ului
+      setTimeout(() => {
+        if (shouldResolve) {
+          resolve({ position, delay });
+        } else {
+          reject({ position, delay });
+        }
+      }, delay);
+    });
+  };
